334523a6882019f37cbeaeb8d227d0e7f6813873
Milan Das <milan@ulap.co>
2020-03-13 22:02:53 -0400

    2020-03-14T02:02:53Z

--- rocket_launches.py
+++ rocket_launches.py
@@ -1,52 +1,47 @@
-import json
-import pathlib
- 
-import airflow
-import requests
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from airflow.operators.python_operator import PythonOperator
- 
+from datetime import datetime, timedelta
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime.utcnow(),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
 dag = DAG(
-   dag_id="rocket_launches",
-   start_date=airflow.utils.dates.days_ago(14),
-   schedule_interval=None,
-)
- 
-download_launches = BashOperator(
-   task_id="download_launches",
-   bash_command="curl -o /tmp/launches.json 'https://launchlibrary.net/1.4/launch?next=5&mode=verbose'",
-   dag=dag,
-)
- 
- 
-def _get_pictures():
-   # Ensure directory exists
-   pathlib.Path("/tmp/images").mkdir(parents=True, exist_ok=True)
- 
-   # Download all pictures in launches.json
-   with open("/tmp/launches.json") as f:
-       launches = json.load(f)
-       image_urls = [launch["rocket"]["imageURL"] for launch in launches["launches"]]
-       for image_url in image_urls:
-           response = requests.get(image_url)
-           image_filename = image_url.split("/")[-1]
-           target_file = f"/tmp/images/{image_filename}"
-           with open(target_file, "wb") as f:
-               f.write(response.content)
-           print(f"Downloaded {image_url} to {target_file}")
- 
- 
-get_pictures = PythonOperator(
-   task_id="get_pictures",
-   python_callable=_get_pictures,
-   dag=dag,
-)
- 
-notify = BashOperator(
-   task_id="notify",
-   bash_command='echo "There are now $(ls /tmp/images/ | wc -l) images."',
-   dag=dag,
-)
- 
-download_launches >> get_pictures >> notify
+    'kubernetes_sample', default_args=default_args, schedule_interval=timedelta(minutes=10))
+
+
+start = DummyOperator(task_id='run_this_first', dag=dag)
+
+passing = KubernetesPodOperator(namespace='default',
+                          image="Python:3.6",
+                          cmds=["Python","-c"],
+                          arguments=["print('hello world')"],
+                          labels={"foo": "bar"},
+                          name="passing-test",
+                          task_id="passing-task",
+                          get_logs=True,
+                          dag=dag
+                          )
+
+failing = KubernetesPodOperator(namespace='default',
+                          image="ubuntu:1604",
+                          cmds=["Python","-c"],
+                          arguments=["print('hello world')"],
+                          labels={"foo": "bar"},
+                          name="fail",
+                          task_id="failing-task",
+                          get_logs=True,
+                          dag=dag
+                          )
+
+passing.set_upstream(start)
+failing.set_upstream(start)
