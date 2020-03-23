f8836652718b9ac2db854220d112da37cf0d39e4
Milan Das <milan@ulap.co>
2020-03-22 22:20:34 -0400

    2020-03-23T02:20:33Z

--- sftp_to_gcs.py
+++ sftp_to_gcs.py
@@ -8,12 +8,12 @@ dag = DAG(
     schedule_interval='@daily'
 )
 
-put_operation = SFTPOperator(
-    task_id="operation",
+get_operation = SFTPOperator(
+    task_id="put_sftp",
     ssh_conn_id="ssh_default",
-    local_filepath="route_to_local_file",
-    remote_filepath="remote_route_to_copy",
-    operation="put",
+    local_filepath="/tmp/images",
+    remote_filepath="/upload",
+    operation="get",
     dag=dag
 )
 # get_operation = SFTPOperator(....,
