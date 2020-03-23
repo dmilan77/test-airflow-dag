f1f32e9b99f0497a7160af112f00c123b0132c59
Milan Das <milan@ulap.co>
2020-03-23 08:50:55 -0400

    2020-03-23T12:50:55Z

--- sftp_to_gcs.py
+++ sftp_to_gcs.py
@@ -6,14 +6,8 @@ import datetime
 
 default_args = {
     'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': days_ago(2),
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-    # 'queue': 'bash_queue',
+    'retries': 1
+     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
